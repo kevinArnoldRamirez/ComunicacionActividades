20488beab4f85a943fe9711ff09be479cb06103e
kevinArnoldRamirez <64047619+kevinArnoldRamirez@users.noreply.github.com>
2020-04-27 12:31:21 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/isc/tesoem/arnold0824/comunicacionactividades/frmUno.java
@@ -0,0 +1,15 @@
+package mx.edu.isc.tesoem.arnold0824.comunicacionactividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class frmUno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_uno);
+    }
+
+}
